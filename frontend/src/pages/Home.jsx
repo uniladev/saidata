25bec1a41dce6f9716d6d18853960af02619bee7
Hafizh <112592675+Taufiqu@users.noreply.github.com>
2025-10-14 18:41:04 +0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,5 @@
 // src/pages/Home.jsx
 import { HeroCarousel, Features, About, News } from "../components/sections";
-import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
