dffe7ae960bf6c8fd9e593e0c26c3267481d4d64
dafahan <dafahanid@gmail.com>
2025-10-14 17:11:45 +0700

    update:notfound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ export default function App() {
       <Route
         element={
           <ProtectedRoute>
-            <DashboardLayout />
+            <AuthenticatedLayout />
           </ProtectedRoute>
         }
       >
