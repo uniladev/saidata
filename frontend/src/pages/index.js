1baece6742578153e829b7f0f3f6986c790722e6
dafahan <dafahanid@gmail.com>
2025-10-14 17:03:33 +0700

    update:notfound path

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,6 +1,5 @@
 import { lazy } from "react";
 export const Home = lazy(() => import("./guest/Home.jsx"));
 export const Login = lazy(() => import("./auth/Login.jsx"));
-export const NotFound = lazy(() => import("./guest/NotFound.jsx"));
 export const About = lazy(() => import("./guest/About.jsx"));
 
