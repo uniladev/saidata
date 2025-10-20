8574feecedd0e90f202c6137b59e97b9979e076f
Enggal0 <enggalbimas8@gmail.com>
2025-10-20 15:58:24 +0700

    allow all cors for dev

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5173',
+        '*',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
